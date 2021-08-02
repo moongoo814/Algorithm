8fe6c58741f24d227e5f1dea540b93f9732158d4
moongoo814 <75865580+moongoo814@users.noreply.github.com>
2021-08-02 14:10:58 +0900

    Create hanoi.py

---
+++ hanoi.py
@@ -0,0 +1,15 @@
+#-*- coding: cp949 -*-
+#-*- coding: utf-8 -*-
+
+def move(no: int, x: int, y:int )-> None:
+    if no > 1 :
+        move(no-1, x,6-x-y)
+
+    print(f"원반 [{no}]을 {x}기둥에서 {y}기둥으로 옮깁니다")
+
+    if no> 1:
+        move(no-1, 6-x-y, y)
+print("하노이의 탑을 구현합니다")
+n = int(input("원반의 개수를 입력하세요"))
+
+move(n,1,3)
